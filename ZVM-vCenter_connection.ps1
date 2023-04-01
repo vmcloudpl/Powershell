24975248414302f2db811566d29060468d6fe794
vmcloudpl <129306006+vmcloudpl@users.noreply.github.com>
2023-04-01 12:40:29 +0200

    Add files via upload

---
+++ ZVM-vCenter_connection.ps1
@@ -0,0 +1,9 @@
+﻿#vmcloud.pl
+if ((Test-NetConnection -ComputerName YOUR_vCenter_IP -Port 443).TcpTestSucceeded){
+        Write-Host "vCenter is up" -ForegroundColor Green
+        }
+    else
+    {
+        Write-Host "vCenter is down" -ForegroundColor Red
+        Write-EventLog -LogName "Application" -Source "Zerto" -EventID 16 -EntryType Error -Message "Connection with vCenter over port 443 is down"
+    }
---
+++ ZVM-vCenter_connection_task.ps1
@@ -0,0 +1,5 @@
+﻿#vmcloud.pl
+$taskName = "ZVM-vCenter Connection"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "C:\temp\vCenter_connection.ps1"
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -RunLevel Highest -User "System"
